6e83047ef681c9b86426916aefcf627ee5969e11
Giandonn <lucas_raineri04@hotmail.com>
2024-05-28 22:03:21 -0300

    mapa funcionando

--- src/components/newMap.vue
+++ src/components/newMap.vue
@@ -1,63 +1,106 @@
 <template>
-    <div id="map">
-      <div id="mapContainer" style="height:600px;width:100%" ref="hereMap"></div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "HereMap",
-    props: {
-      center: Object
+  <div id="map">
+    <div id="mapContainer" style="height:600px;width:100%" ref="hereMap"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HereMap",
+  props: {
+    start: {
+      type: Object,
+      required: true
     },
-    data() {
-      return {
-        platform: null,
-        apikey: "eGEbMqmjPEdw473hAUUXR5t_22Ys36iC6n4NfKGCu8Q"
-      };
-    },
-    async mounted() {
-      const platform = new window.H.service.Platform({
-        apikey: this.apikey
-      });
-      this.platform = platform;
-      this.initializeHereMap();
-    },
-    methods: {
-        initializeHereMap() { 
-  const mapContainer = this.$refs.hereMap;
-  const H = window.H;
-  const maptypes = this.platform.createDefaultLayers();
-  
-  const unimarCoordinates = {
+    end: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      platform: null,
+      apikey: "eGEbMqmjPEdw473hAUUXR5t_22Ys36iC6n4NfKGCu8Q"
+    };
+  },
+  async mounted() {
+    const platform = new window.H.service.Platform({
+      apikey: this.apikey
+    });
+    this.platform = platform;
+    this.initializeHereMap(this.start, this.end);
+  },
+  methods: {
+    initializeHereMap(start, end) { 
+      const mapContainer = this.$refs.hereMap;
+      const H = window.H;
+      const maptypes = this.platform.createDefaultLayers();
+
+
+      const unimarCoordinates = {
     lat: -22.23513460978872,
     lng: -49.96971069370079           
   };
+      const map = new H.Map(mapContainer, maptypes.vector.normal.map, {
+        zoom: 16,
+        center: unimarCoordinates
+      });
 
-  const map = new H.Map(mapContainer, maptypes.vector.normal.map, {
-    zoom: 16,
-    center: unimarCoordinates
-  });
+      const icon = new H.map.Icon('https://cdn-icons-png.flaticon.com/512/5817/5817087.png', {
+        size: { w: 50, h: 50 }
+      });
 
-  const icon = new H.map.Icon('https://cdn-icons-png.flaticon.com/512/5817/5817087.png', {
-    size: { w: 50, h: 50 }
-  });
+      const startMarker = new H.map.Marker(start, { icon: icon });
+      map.addObject(startMarker);
 
-  const marker = new H.map.Marker(unimarCoordinates, { icon: icon });
-  map.addObject(marker);
+      const startLabel = new H.map.DomIcon(`<div style="color: blue;">Start</div>`);
+      const startLabelMarker = new H.map.DomMarker(start, { icon: startLabel });
+      map.addObject(startLabelMarker);
 
-  const label = new H.map.DomIcon(`<div style="color: blue;">Unimar</div>`);
-  const labelMarker = new H.map.DomMarker(unimarCoordinates, { icon: label });
-  map.addObject(labelMarker);
+      const endMarker = new H.map.Marker(end, { icon: icon });
+      map.addObject(endMarker);
 
-  addEventListener("resize", () => map.getViewPort().resize());
+      const endLabel = new H.map.DomIcon(`<div style="color: red;">End</div>`);
+      const endLabelMarker = new H.map.DomMarker(end, { icon: endLabel });
+      map.addObject(endLabelMarker);
 
-  new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+      addEventListener("resize", () => map.getViewPort().resize());
 
-  H.ui.UI.createDefault(map, maptypes);
-}
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
+      H.ui.UI.createDefault(map, maptypes);
+
+      this.addRoute(map, start, end);
+    },
+    addRoute(map, start, end) {
+      const router = this.platform.getRoutingService(null, 8);
+
+      const routeRequestParams = {
+        routingMode: 'fast',
+        transportMode: 'car',
+        origin: `${start.lat},${start.lng}`,
+        destination: `${end.lat},${end.lng}`,
+        return: 'polyline'
+      };
+
+      router.calculateRoute(routeRequestParams, result => {
+        if (result.routes.length) {
+          result.routes[0].sections.forEach(section => {
+            const linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
+
+            const routeLine = new H.map.Polyline(linestring, {
+              style: { strokeColor: 'blue', lineWidth: 5 }
+            });
+
+            map.addObject(routeLine);
+
+            map.getViewModel().setLookAtData({ bounds: routeLine.getBoundingBox() });
+          });
+        }
+      }, error => {
+        console.error(error);
+      });
     }
-  };
-  </script>
-  
+  }
+};
+</script>
